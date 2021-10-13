00e2b44b150b30f36d0b88ba826fa3a58507dba1
Aniket Pandharabale <aniketicloud@gmail.com>
2021-10-13 05:30:54 +0530

    solved url issue after navigation

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,17 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
 
+  const history = useHistory();
+  console.log({ history });
+
   useEffect(() => {
     mount(ref.current, {
-      onNavigate: () => {
-        console.log('The container noticed navigation in marketing');
+      onNavigate: ({ pathname: nextPathname }) => {
+        history.push(nextPathname);
       },
     });
   });
