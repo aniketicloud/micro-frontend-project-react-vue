ee62e24f095832442a3e80b676240a761394c004
Aniket Pandharabale <aniketicloud@gmail.com>
2021-10-16 21:52:17 +0530

    integrating auth into container

---
+++ packages/container/src/components/AuthApp.js
@@ -0,0 +1,27 @@
+import { mount } from 'auth/AuthApp';
+import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default () => {
+  const ref = useRef(null);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        // nextPathname is path inside remote app i.e. marketing app
+        // this below pathname is current path inside container app
+        const { pathname } = history.location;
+
+        // to remove infinite loop, adding condition
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
+  return <div ref={ref} />;
+};
