c0d222e35f3cc122d31745e0e9db63708795fa05
Aniket Pandharabale <aniketicloud@gmail.com>
2021-10-16 22:42:36 +0530

    Lazy loading of remotes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
-import AuthApp from './components/AuthApp';
-import Header from './components/Header';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {
   StylesProvider,
   createGenerateClassName,
 } from '@material-ui/core/styles';
+
+import Header from './components/Header';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 export default () => {
   const generateClassName = createGenerateClassName({
@@ -17,10 +19,12 @@ export default () => {
       <BrowserRouter>
         <StylesProvider generateClassName={generateClassName}>
           <Header />
-          <Switch>
-            <Route path="/auth" component={AuthApp} />
-            <Route path="/" component={MarketingApp} />
-          </Switch>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </StylesProvider>
       </BrowserRouter>
     </div>
