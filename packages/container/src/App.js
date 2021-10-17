188d41c6b5eb13757dd9326e3cbeab306ec54175
Aniket Pandharabale <aniketicloud@gmail.com>
2021-10-17 21:11:24 +0530

    Protecting access to Dashboard
    Not perfect, but I am learning micro frontends, not Authentication

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,11 @@
-import React, { lazy, Suspense, useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { lazy, Suspense, useState, useEffect } from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import {
   StylesProvider,
   createGenerateClassName,
 } from '@material-ui/core/styles';
+
+import { createBrowserHistory } from 'history';
 
 import Progress from './components/Progress';
 import Header from './components/Header';
@@ -16,11 +18,26 @@ const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 });
 
+// ! accsing history from <BrowserHistory /> is giving so many challenges, specifically for this component
+// ? read how <BrowserHistory / works and how <Router/> creates history
+// Thats' why I am creating history manually, bringing that into generic <Router/>
+// So, This is more related to 'react-router-dom', and nothing to micro frontend
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
+
+  // with useEffect, whenever second argument changes i.e. 'isSignedIn' changes,
+  // first argument i.e. arrow function will be run
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push('/dashboard');
+    }
+  }, [isSignedIn]);
+
   return (
     <div>
-      <BrowserRouter>
+      <Router history={history}>
         <StylesProvider generateClassName={generateClassName}>
           <Header
             onSignOut={() => setIsSignedIn(false)}
@@ -32,13 +49,14 @@ export default () => {
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
               <Route path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
                 <DashboardLazy />
               </Route>
               <Route path="/" component={MarketingLazy} />
             </Switch>
           </Suspense>
         </StylesProvider>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
