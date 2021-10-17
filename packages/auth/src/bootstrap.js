9aafe71393a4eac30a87305b3fd2b7b07eb138cf
Aniket Pandharabale <aniketicloud@gmail.com>
2021-10-17 16:12:33 +0530

    more configuration

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -26,7 +26,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath, onSignIn }) => {
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#auth-dev-root');
+  const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -5,9 +5,11 @@ import App from './App';
 
 // Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory, initialPath, onSignIn }) => {
-  const history = defaultHistory || createMemoryHistory({
-    initialEntries: [initialPath]
-  });
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
   if (onNavigate) {
     history.listen(onNavigate);
   }
@@ -26,7 +28,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath, onSignIn }) => {
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#auth-dev-root');
+  const devRoot = document.querySelector('#_dashboard-dev-root');
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
